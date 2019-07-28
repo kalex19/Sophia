4aaef11ce1a2d9e6a619099388606fa89e417313
Katherine Lewis <kale8432@colorado.edu>
2019-07-28 14:16:57 -0600

    built out test suite for server, untested yet

--- sophia-server/server.js
+++ sophia-server/server.js
@@ -3,40 +3,40 @@
 
 const express = require('express')
 const cors = require('cors')
-const app = express()
+const server = express()
 const port = 3000
 
-app.use(cors());
-app.use(express.json()); 
+server.use(cors());
+server.use(express.json()); 
 
-app.get('/', (req, res) => res.send('yo!'));
+server.get('/', (req, res) => res.send('yo!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+server.listen(port, () => console.log(`Example server listening on port ${port}!`));
 
-app.use(express.static('public'));
+server.use(express.static('public'));
 
-app.locals.lists = [];
-app.locals.items = []; 
+server.locals.lists = [];
+server.locals.items = []; 
 
 
 //Routes
 
 //Get All Lists
-app.get("/api/v1/lists", (req, res) => {  
-  res.status(200).json(app.locals.lists);
+server.get("/api/v1/lists", (req, res) => {  
+  res.status(200).json(server.locals.lists);
 });
 
 //Get Specific List
-app.get("/api/v1/lists/:id", (req, res) => {
+server.get("/api/v1/lists/:id", (req, res) => {
   const { id } = req.params;
-  const list = app.locals.lists.find(list => list.id == id);
+  const list = server.locals.lists.find(list => list.id == id);
   if (!list) return res.status(404).json("List not found");
   return res.status(200).json(list);
 });
 
 //Post a Specific List
 
-app.post("/api/v1/lists", (req, res) => {
+server.post("/api/v1/lists", (req, res) => {
   const { title } = req.body;
   if (!title)
     return res.status(422).json("Please provide a list title");
@@ -45,50 +45,50 @@ app.post("/api/v1/lists", (req, res) => {
     //should not use date.now
     ...req.body
   };
-  app.locals.lists.push(newList);
+  server.locals.lists.push(newList);
   res.status(201).json(newList);
 });
 
 //Update Specific List
 
-app.put("/api/v1/lists/:id", (req, res) => {
+server.put("/api/v1/lists/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const { title } = req.body;
   if (!title)
     return res.status(422).json("Please provide a list title");
-  const listIndex = app.locals.lists.findIndex(list => list.id === id);
+  const listIndex = server.locals.lists.findIndex(list => list.id === id);
   if (listIndex === -1) return res.status(404).json("List not found");
   const updatedList = { id, title };
-  app.locals.lists.splice(listIndex, 1, updatedList);
+  server.locals.lists.splice(listIndex, 1, updatedList);
   return res.sendStatus(204);
 });
 
 //Delete Specific List
 
-app.delete("/api/v1/lists/:id", (req, res) => {
+server.delete("/api/v1/lists/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const listIndex = app.locals.lists.findIndex(list => list.id === id);
+  const listIndex = server.locals.lists.findIndex(list => list.id === id);
   if (listIndex === -1) return res.status(404).json("List does not exist");
-  app.locals.lists.splice(listIndex, 1);
+  server.locals.lists.splice(listIndex, 1);
   return res.sendStatus(204);
 });
 
 //Get All Items
-app.get("/api/v1/items", (req, res) => {  
-  res.status(200).json(app.locals.items);
+server.get("/api/v1/items", (req, res) => {  
+  res.status(200).json(server.locals.items);
 });
 
 //Get Specific Item
-app.get("/api/v1/items/:id", (req, res) => {
+server.get("/api/v1/items/:id", (req, res) => {
   const { id } = req.params;
-  const item = app.locals.items.find(item => item.id == id);
+  const item = server.locals.items.find(item => item.id == id);
   if (!item) return res.status(404).json("Item not found");
   return res.status(200).json(item);
 });
 
 //Post a Specific Item
 
-app.post("/api/v1/items", (req, res) => {
+server.post("/api/v1/items", (req, res) => {
   const { task  } = req.body;
   if (!task)
     return res.status(422).json("Please provide an item task");
@@ -97,31 +97,31 @@ app.post("/api/v1/items", (req, res) => {
     //should not use date.now
     ...req.body
   };
-  app.locals.items.push(newItem);
+  server.locals.items.push(newItem);
   res.status(201).json(newItem);
 });
 
 //Update Specific Item
 
-app.put("/api/v1/items/:id", (req, res) => {
+server.put("/api/v1/items/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const { task } = req.body;
   if (!task)
     return res.status(422).json("Please provide an item task");
-  const itemIndex = app.locals.items.findIndex(item => item.id === id);
+  const itemIndex = server.locals.items.findIndex(item => item.id === id);
   if (itemIndex === -1) return res.status(404).json("Item not found");
   const updatedItem = { id, title };
-  app.locals.items.splice(itemIndex, 1, updatedItem);
+  server.locals.items.splice(itemIndex, 1, updatedItem);
   return res.sendStatus(204);
 });
 
 //Delete Specific List
 
-app.delete("/api/v1/items/:id", (req, res) => {
+server.delete("/api/v1/items/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const itemIndex = app.locals.items.findIndex(item => item.id === id);
+  const itemIndex = server.locals.items.findIndex(item => item.id === id);
   if (itemIndex === -1) return res.status(404).json("Item does not exist");
-  app.locals.items.splice(itemIndex, 1);
+  server.locals.items.splice(itemIndex, 1);
   return res.sendStatus(204);
 });
 
